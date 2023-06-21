cb8e045577638d8b77fa5ca4f003be1b8a36c360
miketeow <27071053+miketeow@users.noreply.github.com>
2023-06-21 18:06:28 +0800

    first commit

---
+++ game_logics/ship_methods.js
@@ -0,0 +1,32 @@
+function checkForShip(player, coordinates) {
+  var shipPresent, ship;
+
+  for (var i = 0; i < player.ships.length; i++) {
+    ship = player.ships[i];
+
+    shipPresent = ship.locations.filter(function (actualCoordinates) {
+      return (
+        actualCoordinates[0] === coordinates[0] &&
+        actualCoordinates[1] === coordinates[1]
+      );
+    })[0];
+    if (shipPresent) {
+      return ship;
+    }
+  }
+
+  return false;
+}
+
+function damageShip(ship, coordinates) {
+  ship.damage.push(coordinates);
+}
+
+function fire(player, coordinates) {
+  var ship = checkForShip(player, coordinates);
+
+  if (ship) {
+    damageShip(ship, coordinates);
+  }
+}
+module.exports = { checkForShip, damageShip, fire };
